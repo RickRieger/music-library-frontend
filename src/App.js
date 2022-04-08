42a19baa2c1ffc59b6fc119cb94175847ff5f8c3
Rick Rieger <rickrieger36251@gmail.com>
2022-04-08 13:43:52 -0400

    added a modal + added a form + added a nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import MusicCollection from "./Components/MusicCollection/MusicCollection";
+import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
+    <NavBar/>
     <MusicCollection/>
     </div>
   );
--- src/Components/MusicCollection/MusicCollection.jsx
+++ src/Components/MusicCollection/MusicCollection.jsx
@@ -3,6 +3,10 @@ import Axios from '../../utils/Axios';
 import { DataGrid } from '@mui/x-data-grid';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
 function MusicCollection() {
   const [songs, setSongs] = useState([]);
 
@@ -19,6 +23,23 @@ function MusicCollection() {
       console.log(e.message);
     }
   };
+
+  const style = {
+    position: 'absolute',
+    top: '35%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 800,
+    height: 400,
+    bgcolor: '#868686',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
@@ -76,15 +97,76 @@ function MusicCollection() {
             checkboxSelection
           />
         </div>
-        <Stack spacing={2} direction='row' style={{
-          display:'flex',
-          justifyContent:'center',
-          marginTop:'50px'
-        }}>
-          <Button variant='contained'>Add Song</Button>
+        <Stack
+          spacing={2}
+          direction='row'
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '50px',
+          }}
+        >
+          <Button variant='contained' onClick={handleOpen}>
+            Add Song
+          </Button>
           <Button variant='contained'>Update Song</Button>
           <Button variant='contained'>Delete Song</Button>
         </Stack>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby='modal-modal-title'
+          aria-describedby='modal-modal-description'
+        >
+          <Box sx={style}>
+            <Typography id='modal-modal-description' style={{fontSize:'2rem', textAlign:'center'}}>
+              Enter Song Info
+            </Typography>
+            <Typography id='modal-modal-title' variant='h6' component='h2'>
+              <Box
+                component='form'
+                sx={{
+                  '& .MuiTextField-root': { mx: 7, mt: 4, width: '25ch' },
+                }}
+                noValidate
+                autoComplete='off'
+              >
+                <div>
+                  <TextField
+                    id='standard-basic'
+                    label='song title'
+                    variant='standard'
+                  />
+                  <TextField
+                    id='standard-basic'
+                    label='artist'
+                    variant='standard'
+                  />
+                  <TextField
+                    id='standard-basic'
+                    label='album'
+                    variant='standard'
+                  />
+                  <TextField
+                    id='standard-basic'
+                    label='release date'
+                    variant='standard'
+                  />
+                  <TextField
+                    id='standard-basic'
+                    label='genre'
+                    variant='standard'
+                  />
+                </div>
+              </Box>
+            </Typography>
+            <Typography id='modal-modal-description' style={{textAlign:'center'}} sx={{ mt: 10}}>
+              <Button style={{ width: '6rem' }} variant='contained'>
+                add
+              </Button>
+            </Typography>
+          </Box>
+        </Modal>
       </Fragment>
     );
   }
